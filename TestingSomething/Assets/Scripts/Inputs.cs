9017e449b8e3505f4bfda7523ef8f7ec92b9ef48
Spoutnique <spootnique@gmail.com>
2016-03-28 14:54:39 +0200

    1stPush

---
+++ TestingSomething/Assets/Scripts/Inputs.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Inputs : MonoBehaviour {
+
+
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+
+		//print whatever button is pressed
+		/*for (int i = 0;i < 20; i++) {
+			if(Input.GetKeyDown("joystick 1 button "+i)){
+				print("joystick 1 button "+i);
+			}
+		}*/
+	
+		Debug.DrawLine(transform.position,transform.forward, Color.red);
+		if(Input.GetKeyDown("joystick 1 button "+0) && CrossPlatformInputManager.GetAxis("Horizontal") == 0 && CrossPlatformInputManager.GetAxis("Vertical") == 0 ){ //if you press A & not moving
+			transform.position = new Vector3(0,0,0);//reset your position
+		}
+
+	}//end of update
+
+}
---
+++ TestingSomething/Assets/Scripts/MoveFPSController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class MoveFPSController : MonoBehaviour {
+	public GameObject FpsController;
+
+
+	void Start () {
+	
+	}
+
+
+	void Update () {
+		//FpsController.transform.Translate(CrossPlatformInputManager.GetAxis("Horizontal")*0.1f,0,CrossPlatformInputManager.GetAxis("Vertical")*0.1f,spa);
+	}
+}
